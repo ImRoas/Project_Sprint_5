711a53e45e7c986fdd6fab6ace0202f23e0a0909
ROAS <161911242+ImRoas@users.noreply.github.com>
2024-03-04 15:10:47 -0600

    initial commit

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-#st.header('Lanzar una moneda')  
+st.header('Gráficos de autos')  
+st.write('Lalalala.') 
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
-
-#st.write('Esta aplicación aún no es funcional. En construcción.') 
 
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
