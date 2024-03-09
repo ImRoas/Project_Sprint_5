304cc9e8b3be4543f99626d23d6855dfe632efa1
ROAS <161911242+ImRoas@users.noreply.github.com>
2024-03-09 15:44:57 -0600

    initial commit

--- app.py
+++ app.py
@@ -14,7 +14,11 @@ if hist_button: # al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(
+        car_data, 
+        x="odometer"
+        color='model'
+    )
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
