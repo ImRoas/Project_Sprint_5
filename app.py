9bd00b9066d3261570c6a5eab59187a32a93134a
ROAS <161911242+ImRoas@users.noreply.github.com>
2024-03-09 18:15:45 -0600

    initial commit

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+car_data['model_year'] = car_data['model_year'].fillna(value='')
+car_data['model_year'] = car_data['model_year'].replace('', 0)
 car_data['model_year'] = car_data['model_year'].astype('int')
 
 st.header('Gráficos de autos')  
